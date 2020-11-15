2454f5d866c52b6b2d71dd5f8c20747f3930fa1b
Benardakaka <benardakaka484@gmail.com>
2020-11-15 04:12:38 +0300

    add

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: false,
+  TrendingUrl: 'https://api.giphy.com/v1/gifs/trending?api_key=iXnp72e4U2TNlfyWd2Wj9IHLxBiTKbMM&limit=20&rating=g',
+  SearchUrl: 'https://api.giphy.com/v1/gifs/search?api_key=iXnp72e4U2TNlfyWd2Wj9IHLxBiTKbMM'
+
 };
+
